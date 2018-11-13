f2c7a615a4ab192d099259a92f0ed1b617f5762b
gracri <chrisgraff@protonmail.com>
2018-11-13 01:43:48 +0100

    Flow

---
+++ cordapp/src/main/java/com/template/IOUFlow.java
@@ -0,0 +1,69 @@
+package com.template;
+
+import co.paralleluniverse.fibers.Suspendable;
+import net.corda.core.flows.FlowException;
+import net.corda.core.flows.FlowLogic;
+import net.corda.core.flows.InitiatingFlow;
+import net.corda.core.flows.StartableByRPC;
+import net.corda.core.utilities.ProgressTracker;
+import net.corda.core.flows.FinalityFlow;
+
+import net.corda.core.contracts.Command;
+import net.corda.core.contracts.CommandData;
+import net.corda.core.identity.Party;
+import net.corda.core.transactions.SignedTransaction;
+import net.corda.core.transactions.TransactionBuilder;
+
+
+// ******************
+// * Initiator flow *
+// ******************
+@InitiatingFlow
+@StartableByRPC
+public class IOUFlow extends FlowLogic<Void> {
+    private final Integer iouValue;
+    private final Party otherParty;
+
+    /**
+     * The progress tracker provides checkpoints indicating the progress of the flow to observers.
+     */
+    private final ProgressTracker progressTracker = new ProgressTracker();
+
+    public IOUFlow(Integer iouValue, Party otherParty) {
+        this.iouValue = iouValue;
+        this.otherParty = otherParty;
+    }
+
+    @Override
+    public ProgressTracker getProgressTracker() {
+        return progressTracker;
+    }
+
+    /**
+     * The flow logic is encapsulated within the call() method.
+     */
+    @Suspendable
+    @Override
+    public Void call() throws FlowException {
+        // We retrieve the notary identity from the network map.
+        final Party notary = getServiceHub().getNetworkMapCache().getNotaryIdentities().get(0);
+
+        // We create the transaction components.
+        IOUState outputState = new IOUState(iouValue, getOurIdentity(), otherParty);
+        CommandData cmdType = new TemplateContract.Commands.Action();
+        Command cmd = new Command<>(cmdType, getOurIdentity().getOwningKey());
+
+        // We create a transaction builder and add the components.
+        final TransactionBuilder txBuilder = new TransactionBuilder(notary)
+                .addOutputState(outputState, TemplateContract.ID)
+                .addCommand(cmd);
+
+        // Signing the transaction.
+        final SignedTransaction signedTx = getServiceHub().signInitialTransaction(txBuilder);
+
+        // Finalising the transaction.
+        subFlow(new FinalityFlow(signedTx));
+
+        return null;
+    }
+}
