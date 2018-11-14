2a1c1fffeebee8d458c1b0af743b8a5dbc711558
Christine Graff <chrisgraff@protonmail.com>
2018-11-14 13:28:10 +0100

    Responder flow class

--- cordapp/src/main/java/com/template/IOUFlow.java
+++ cordapp/src/main/java/com/template/IOUFlow.java
@@ -1,18 +1,17 @@
 package com.template;
 
 import co.paralleluniverse.fibers.Suspendable;
-import net.corda.core.flows.FlowException;
-import net.corda.core.flows.FlowLogic;
-import net.corda.core.flows.InitiatingFlow;
-import net.corda.core.flows.StartableByRPC;
-import net.corda.core.utilities.ProgressTracker;
-import net.corda.core.flows.FinalityFlow;
-
+import java.util.Arrays;
 import net.corda.core.contracts.Command;
-import net.corda.core.contracts.CommandData;
+import net.corda.core.contracts.StateAndContract;
+import net.corda.core.flows.*;
 import net.corda.core.identity.Party;
 import net.corda.core.transactions.SignedTransaction;
 import net.corda.core.transactions.TransactionBuilder;
+import net.corda.core.utilities.ProgressTracker;
+
+import java.security.PublicKey;
+import java.util.List;
 
 
 // ******************
@@ -48,21 +47,34 @@ public class IOUFlow extends FlowLogic<Void> {
         // We retrieve the notary identity from the network map.
         final Party notary = getServiceHub().getNetworkMapCache().getNotaryIdentities().get(0);
 
+        // We create a transaction builder.
+        final TransactionBuilder txBuilder = new TransactionBuilder();
+        txBuilder.setNotary(notary);
+
         // We create the transaction components.
         IOUState outputState = new IOUState(iouValue, getOurIdentity(), otherParty);
-        CommandData cmdType = new TemplateContract.Commands.Action();
-        Command cmd = new Command<>(cmdType, getOurIdentity().getOwningKey());
+        StateAndContract outputContractAndState = new StateAndContract(outputState, IOUContract.ID);
+        List<PublicKey> requiredSigners = Arrays.asList(getOurIdentity().getOwningKey(), otherParty.getOwningKey());
+        Command cmd = new Command<>(new IOUContract.Create(), requiredSigners);
 
-        // We create a transaction builder and add the components.
-        final TransactionBuilder txBuilder = new TransactionBuilder(notary)
-                .addOutputState(outputState, TemplateContract.ID)
-                .addCommand(cmd);
+        // We add the items to the builder.
+        txBuilder.withItems(outputContractAndState, cmd);
+
+        // Verifying the transaction.
+        txBuilder.verify(getServiceHub());
 
         // Signing the transaction.
         final SignedTransaction signedTx = getServiceHub().signInitialTransaction(txBuilder);
 
+        // Creating a session with the other party.
+        FlowSession otherpartySession = initiateFlow(otherParty);
+
+        // Obtaining the counterparty's signature.
+        SignedTransaction fullySignedTx = subFlow(new CollectSignaturesFlow(
+                signedTx, Arrays.asList(otherpartySession), CollectSignaturesFlow.tracker()));
+
         // Finalising the transaction.
-        subFlow(new FinalityFlow(signedTx));
+        subFlow(new FinalityFlow(fullySignedTx));
 
         return null;
     }
